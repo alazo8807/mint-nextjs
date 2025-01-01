83ca813c195711b40fb1c9b27044a878c29f447c
alazo8807 <alazo8807@gmail.com>
2025-01-01 16:53:52 -0500

    improve category rendering

--- app/transactions/TableRow.tsx
+++ app/transactions/TableRow.tsx
@@ -9,8 +9,15 @@ type Transaction = {
   type TableRowProps = {
     transaction: Transaction;
   };
-  
+
+  // Category is a string that contains an array of categories(e.g ["Travel","Taxi"]).
+  // We need to parse it to an array and display only the last category.
   export default function TableRow({ transaction }: TableRowProps) {
+    const renderCategory = (category: string): string => {
+      const categoriesArr = JSON.parse(category);
+      return categoriesArr[categoriesArr.length - 1];
+    }
+
     return (
       <tr className="hover:bg-gray-50">
         <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
@@ -21,7 +28,7 @@ type Transaction = {
         </td>
         <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
           <span className="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-gray-100 text-gray-800">
-            {transaction.category}
+            {renderCategory(transaction.category)}
           </span>
         </td>
         <td
